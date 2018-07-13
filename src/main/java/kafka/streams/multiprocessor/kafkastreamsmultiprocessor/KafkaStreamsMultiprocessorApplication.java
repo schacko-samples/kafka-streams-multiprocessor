1919509c40c11d91b8faf587f9b5206af5d605ae
Soby Chacko <schacko@pivotal.io>
2018-07-12 21:05:04 -0400

    Initial commit

---
+++ src/main/java/kafka/streams/multiprocessor/kafkastreamsmultiprocessor/KafkaStreamsMultiprocessorApplication.java
@@ -0,0 +1,12 @@
+package kafka.streams.multiprocessor.kafkastreamsmultiprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsMultiprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamsMultiprocessorApplication.class, args);
+	}
+}
---
+++ src/test/java/kafka/streams/multiprocessor/kafkastreamsmultiprocessor/KafkaStreamsMultiprocessorApplicationTests.java
@@ -0,0 +1,16 @@
+package kafka.streams.multiprocessor.kafkastreamsmultiprocessor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaStreamsMultiprocessorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
